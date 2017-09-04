c0f5fbf9369b4cf0e0df2f1f0cfcf4b02cf8fbd8
dmenagae <dmenagae@gmail.com>
2017-09-03 20:18:32 -0700

    Adding CustomerDAO and CustomerDAOImpl

---
+++ src/com/personaltrainer/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.personaltrainer.dao;
+
+import java.util.List;
+
+import com.personaltrainer.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
